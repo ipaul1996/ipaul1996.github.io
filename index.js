e73c42f1774e9c58ec2063670d5aac82edaaea58
Indrajit <indraindrani1999@gmail.com>
2022-09-15 03:31:49 +0530

    Added portfolio  work  upto 14th September

---
+++ index.js
@@ -0,0 +1,28 @@
+// var icon = document.getElementById("icon");
+
+// icon.onclick = function() {
+
+//     document.body.classList.toggle("dark-theme");
+
+//     if(document.body.classList.contains("dark-theme")) {
+
+//         icon.src = "./sun.png";
+//         document.getElementById('title').style.color = "rgb(243, 169, 19)";
+
+//     } else {
+
+//         icon.src = "./moon.png";
+//         document.getElementById('title').style.color = "#0c0ce2";
+
+//     }
+
+// }
+
+
+document.getElementById('icon2').addEventListener("click",()=> {
+    window.location.href = "https://github.com/ipaul1996";
+});
+
+document.getElementById('icon3').addEventListener("click",()=> {
+    window.location.href = "https://www.linkedin.com/in/indrajitpaul1996/";
+});
